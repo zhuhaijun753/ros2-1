5fb1ef43f02673fc71516f199415ff020fe42722
asobhy <asobhy@blackberry.com>
2020-07-30 09:23:18 -0400

    colcon prefix path fix in setup.sh and local_setup.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Set this variable according to the path of package on target
 ROS2_PACKAGE_TARGET_INSTALL_PATH=/opt/ros/foxy
 
-# Set those variables according to the location of ROS2 Dependencies on host
+# Set those variables according to the location of ROS2 Python Dependencies on host
 ROS2_PYTHON_LIBS=$HOME/workspace/ros2/ros2_dashing/external/lib/python3.8/site-packages
 ROS2_COLCON_PATH=$HOME/workspace/ros2/ros2_dashing/external/bin
 
@@ -32,9 +32,9 @@ for arch in armv7 aarch64 x86_64; do
     echo "CPUVARDIR set to $CPUVARDIR"
 
     FLAGS="-Wl,-rpath-link,$QNX_STAGE/$CPUVARDIR/usr/lib:$PWD/install/$CPUVARDIR/lib \
-                -D__USESRCVERSION -D_QNX_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE \
-                -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes \
-                -I$QNX_STAGE/usr/include"
+            -D__USESRCVERSION -D_QNX_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE \
+            -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes \
+            -I$QNX_STAGE/usr/include"
 
     LDFLAGS="-Wl,--build-id=md5"
 
@@ -60,6 +60,19 @@ for arch in armv7 aarch64 x86_64; do
                         -DBUILD_TESTING:BOOL="OFF" \
                         -DCMAKE_BUILD_TYPE="Release" \
                         -DTARGET_INSTALL_DIR="/opt/ros/foxy"
+
+
+    # The three variables below are patched according to the installation of ros2 on target and the installation of the current package on target
+    # Patching the scripts is done during the build process for convenience but can also be done on target if user chooses to do so
+    # _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX --> package path on target
+    # COLCON_CURRENT_PREFIX --> ros2 path on target
+    # _colcon_prefix_sh_COLCON_CURRENT_PREFIX --> package path on target
+
+    # setup.sh
+    grep -rl "_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=" install/$CPUVARDIR/setup.sh | xargs sed -i "s|_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/.*$|_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=${ROS2_PACKAGE_TARGET_INSTALL_PATH}|g"
+    grep -rl "COLCON_CURRENT_PREFIX=\"/" install/$CPUVARDIR/setup.sh | xargs sed -i 's|COLCON_CURRENT_PREFIX="/.*$|COLCON_CURRENT_PREFIX="/opt/ros/dashing"|g'
+    # local_setup.sh
+    grep -rl "_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"/" install/$CPUVARDIR/local_setup.sh | xargs sed -i "s|_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"/.*\"|_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"${ROS2_PACKAGE_TARGET_INSTALL_PATH}\"|g"
 
 done
 
