a373fcfde75f323ede559d08da5c1ae3ad07f30c
asobhy <asobhy@blackberry.com>
2020-08-10 19:52:39 -0400

    vendor packages fix

--- build.sh
+++ build.sh
@@ -3,76 +3,56 @@
 # Set this variable according to the path of package on target
 ROS2_PACKAGE_TARGET_INSTALL_PATH=/opt/ros/foxy
 
-# Set those variables according to the location of ROS2 Python Dependencies on host
-ROS2_PYTHON_LIBS=$HOME/workspace/ros2/ros2_dashing/external/lib/python3.8/site-packages
-ROS2_COLCON_PATH=$HOME/workspace/ros2/ros2_dashing/external/bin
-
-if [ ! -d "$QNX_TARGET" ]; then
+if [ ! -d "${QNX_TARGET}" ]; then
     echo "QNX_TARGET is not set. Exiting..."
     exit 1
 fi
 
-for arch in armv7 aarch64 x86_64; do
+#for arch in armv7 aarch64 x86_64; do
+for arch in aarch64; do
 
-    if [ "$arch" == "aarch64" ]; then
+    if [ "${arch}" == "aarch64" ]; then
         CPUVARDIR=aarch64le
-        CPUVAR=${arch}le
-    elif [ "$arch" == "armv7" ]; then
+        CPUVAR=aarch64le
+    elif [ "${arch}" == "armv7" ]; then
         CPUVARDIR=armle-v7
         CPUVAR=armv7le
-    elif [ "$arch" == "x86_64" ]; then
-        CPUVARDIR=$arch
-        CPUVAR=$arch
+    elif [ "${arch}" == "x86_64" ]; then
+        CPUVARDIR=x86_64
+        CPUVAR=x86_64
     else
         echo "Invalid architecture. Exiting..."
         exit 1
     fi
 
-    echo "CPU set to $CPUVAR"
-    echo "CPUVARDIR set to $CPUVARDIR"
+    echo "CPU set to ${CPUVAR}"
+    echo "CPUVARDIR set to ${CPUVARDIR}"
 
-    FLAGS="-Wl,-rpath-link,$QNX_STAGE/$CPUVARDIR/usr/lib:$PWD/install/$CPUVARDIR/lib \
-            -D__USESRCVERSION -D_QNX_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE \
-            -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes \
-            -I$QNX_STAGE/usr/include"
+    export CPUVARDIR=${CPUVARDIR}
+    export CPUVAR=${CPUVAR}
+    export ARCH=${arch}
 
-    LDFLAGS="-Wl,--build-id=md5"
+    colcon --log-level 8 build --merge-install --cmake-force-configure \
+        --build-base=build/${CPUVARDIR} \
+        --install-base=install/${CPUVARDIR} \
+        --cmake-args \
+            -DCMAKE_TOOLCHAIN_FILE="${PWD}/platform/qnx.nto.toolchain.cmake" \
+            -DCMAKE_VERBOSE_MAKEFILE:BOOL="ON" \
+            -DBUILD_TESTING:BOOL="OFF" \
+            -DCMAKE_BUILD_TYPE="Release" \
+            -DTARGET_INSTALL_DIR="/opt/ros/foxy"
 
-    # ROS2 Dashing python host build tools
-    EXPORT_CMD= export PYTHONPATH=$ROS2_PYTHON_LIBS:$PYTHONPATH && export PATH=$ROS2_COLCON_PATH:$PATH
-    $EXPORT_CMD && colcon --log-level 8 build --merge-install --cmake-force-configure \
-                    --build-base=build/$CPUVARDIR \
-                    --install-base=install/$CPUVARDIR \
-                    --cmake-args \
-                        -DEXTRA_CMAKE_C_FLAGS="$FLAGS" \
-                        -DEXTRA_CMAKE_CXX_FLAGS="$FLAGS -stdlib=libstdc++ -std=c++14" \
-                        -DEXTRA_CMAKE_LINKER_FLAGS="$LDFLAGS -lstdc++" \
-                        -DCPUVARDIR="$CPUVARDIR" \
-                        -DCPUVAR="$CPUVAR" \
-                        -DARCH="$arch" \
-                        -DCMAKE_TOOLCHAIN_FILE="$PWD/platform/qnx.nto.toolchain.cmake" \
-                        -DCMAKE_MAKE_PROGRAM="$QNX_HOST/usr/bin/make" \
-                        -DGIT_EXECUTABLE="/usr/bin/git" \
-                        -DPYTHON_EXECUTABLE="/usr/bin/python" \
-                        -DPYTHON_LIBRARY="$QNX_TARGET/$CPUVARDIR/usr/lib/libpython3.8.so" \
-                        -DPYTHON_INCLUDE_DIR="$QNX_TARGET/$CPUVARDIR/usr/include/python3.8:$QNX_TARGET/usr/include/python3.8:$QNX_STAGE/usr/include/python3.8" \
-                        -DCMAKE_VERBOSE_MAKEFILE:BOOL="ON" \
-                        -DBUILD_TESTING:BOOL="OFF" \
-                        -DCMAKE_BUILD_TYPE="Release" \
-                        -DTARGET_INSTALL_DIR="/opt/ros/foxy"
-
-
-    # The three variables below are patched according to the installation of ros2 on target and the installation of the current package on target
+	# The three variables below are patched according to the installation of ros2 on target and the installation of the current package on target
     # Patching the scripts is done during the build process for convenience but can also be done on target if user chooses to do so
     # _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX --> package path on target
     # COLCON_CURRENT_PREFIX --> ros2 path on target
     # _colcon_prefix_sh_COLCON_CURRENT_PREFIX --> package path on target
 
     # setup.sh
-    grep -rl "_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=" install/$CPUVARDIR/setup.sh | xargs sed -i "s|_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/.*$|_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=${ROS2_PACKAGE_TARGET_INSTALL_PATH}|g"
-    grep -rl "COLCON_CURRENT_PREFIX=\"/" install/$CPUVARDIR/setup.sh | xargs sed -i 's|COLCON_CURRENT_PREFIX="/.*$|COLCON_CURRENT_PREFIX="/opt/ros/dashing"|g'
+    grep -rl "_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=" install/${CPUVARDIR}/setup.sh | xargs sed -i "s|_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/.*$|_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=${ROS2_PACKAGE_TARGET_INSTALL_PATH}|g"
+    grep -rl "COLCON_CURRENT_PREFIX=\"/" install/${CPUVARDIR}/setup.sh | xargs sed -i 's|COLCON_CURRENT_PREFIX="/.*$|COLCON_CURRENT_PREFIX="/opt/ros/dashing"|g'
     # local_setup.sh
-    grep -rl "_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"/" install/$CPUVARDIR/local_setup.sh | xargs sed -i "s|_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"/.*\"|_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"${ROS2_PACKAGE_TARGET_INSTALL_PATH}\"|g"
+    grep -rl "_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"/" install/${CPUVARDIR}/local_setup.sh | xargs sed -i "s|_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"/.*\"|_colcon_prefix_sh_COLCON_CURRENT_PREFIX=\"${ROS2_PACKAGE_TARGET_INSTALL_PATH}\"|g"
 
 done
 
